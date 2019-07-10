0641952c1295582bab03bdfdd901de5906546ebc
Michael Slowik <slowik.michael@gmail.com>
2019-07-10 14:57:15 -0500

    use example file by default

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+APP_FILE=app.yaml
+if [ ! -f "$APP_FILE" ]; then
+    cp "$APP_FILE.example" "$APP_FILE"
+fi
 for i in $(printenv)
 do
 ENV_KEY="\${$(echo $i | cut -d '=' -f1)}"
 ENV_VALUE="$(echo $i | cut -d '=' -f 2-)"
-sed -i "s|$ENV_KEY|$ENV_VALUE|g" app.yaml
+sed -i "s|$ENV_KEY|$ENV_VALUE|g" "$APP_FILE"
 done
