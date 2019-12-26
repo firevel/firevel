28c7ecbab7909a1610b7cd5dc50e258b8105f488
Michael Slowik <slowik.michael@gmail.com>
2019-12-26 12:33:19 -0300

    Laravel 6.5 front-end updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,10 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Firevel</title>
-
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Scripts -->
         <script src="{{ asset('static/js/app.js') }}" defer></script>
