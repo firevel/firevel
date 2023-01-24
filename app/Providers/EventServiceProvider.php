99e2c140faccff8fe78ee6369f7a03ee01c13d01
Shift <shift@laravelshift.com>
2023-01-24 18:14:42 +0000

    Shift service providers

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -12,7 +12,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The event listener mappings for the application.
      *
-     * @var array
+     * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
         Registered::class => [
@@ -27,8 +27,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        parent::boot();
-
         //
     }
 }
