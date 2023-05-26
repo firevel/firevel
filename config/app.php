689471a05ed07938a471368e0c4b68226a9eb798
Michael Slowik <slowik.michael@gmail.com>
2023-05-26 22:21:58 +0200

    Redis alias removed

--- config/app.php
+++ config/app.php
@@ -209,7 +209,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Redis' => Illuminate\Support\Facades\Redis::class,
+        // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
 ];
