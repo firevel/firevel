bf78a44820199dd1f7ff8242e35213f59bffbe19
Michael Slowik <slowik.michael@gmail.com>
2023-08-18 09:28:35 -0500

    l10 sync

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
 ];
