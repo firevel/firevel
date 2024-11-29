6f2ab1a5352e0f58c69811d6b5268c904f38db92
Shift <shift@laravelshift.com>
2024-11-29 15:36:06 +0000

    Shift cleanup

--- config/logging.php
+++ config/logging.php
@@ -1,10 +1,5 @@
 <?php
 
-use Monolog\Handler\NullHandler;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\SyslogUdpHandler;
-use Monolog\Processor\PsrLogMessageProcessor;
-
 return [
 
     'channels' => [
